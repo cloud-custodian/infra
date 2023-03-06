dab851d015d4a1283aab2024da8c580dff5d4733
Kapil Thangavelu <kapilt@gmail.com>
2023-03-06 12:57:27 -0500

    add poetry freeze plugin repo and teams access to several repos (#2)

--- repos/infra.tf
+++ repos/infra.tf
@@ -1,24 +1,47 @@
 resource "github_repository" "infra" {
   name        = "infra"
   description = "project community infrastructure"
-  visibility = "public"
+  visibility  = "public"
 
   allow_merge_commit = false
   allow_rebase_merge = false
   allow_squash_merge = true
 
   squash_merge_commit_title = "PR_TITLE"
-  auto_init = true
-  delete_branch_on_merge = true
+  auto_init                 = true
+  delete_branch_on_merge    = true
 }
 
 
 
 resource "github_branch_protection_v3" "infra_main" {
-  repository     = github_repository.infra.name
-  branch         = "main"
+  repository = github_repository.infra.name
+  branch     = "main"
 
   required_pull_request_reviews {
     required_approving_review_count = 1
   }
 }
+
+
+
+resource "github_team_repository" "infra_access_team_aws" {
+  team_id    = data.github_team.aws.id
+  repository = github_repository.infra.name
+  permission = "maintain"
+}
+
+
+resource "github_team_repository" "infra_access_team_azure" {
+  team_id    = data.github_team.azure.id
+  repository = github_repository.infra.name
+  permission = "maintain"
+}
+
+
+resource "github_team_repository" "infra_access_team_gcp" {
+  team_id    = data.github_team.gcp.id
+  repository = github_repository.infra.name
+  permission = "maintain"
+}
+
---
+++ repos/teams.tf
@@ -0,0 +1,14 @@
+
+
+data "github_team" "aws" {
+  slug = "aws"
+}
+
+data "github_team" "azure" {
+  slug = "azure"
+}
+
+data "github_team" "gcp" {
+  slug = "gcp"
+}
+
