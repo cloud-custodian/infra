fb18d2901badd0e605f890520f7a96811cbf5899
Kapil Thangavelu <kapilt@gmail.com>
2023-06-28 07:21:09 -0700

    releng - update aws oidc for new actions ssl thumbprint

--- github-aws-access/ci-publish-docs.tf
+++ github-aws-access/ci-publish-docs.tf
@@ -75,7 +75,7 @@ resource "aws_iam_policy" "ci_access" {
 
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
   client_id_list  = ["sts.amazonaws.com"]
 }
 
